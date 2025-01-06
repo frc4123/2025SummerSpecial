2132b3902953ac2a6655946b33c76827c7f3d91d
AlphaLQ <sjb2019dpo@gmail.com>
2025-01-06 11:16:46 -0800

    inital commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,34 @@
+package frc.robot;
+
+public class Constants {
+    
+    public static final class CanId { 
+
+        public static final int Front_Left_Drive = 2;
+        public static final int Front_Right_Drive = 3;
+        public static final int Back_Left_Drive = 4;
+        public static final int Back_Right_Drive = 5;
+        // drive motors - order - start top left in clockwise rotation
+
+        public static final int Front_Left_Turn = 6;
+        public static final int Front_Right_Turn = 7;
+        public static final int Back_Left_Turn = 8;
+        public static final int Back_Right_Turn = 9;
+        // turn motors - order - start top left in clockwise rotation
+
+        public static final int Pigeon = 10;
+
+        public static final int Front_Left_Drive_CANcoder = 11;
+        public static final int Front_Right_Drive_CANcoder = 12;
+        public static final int Back_Left_Drive_CANcoder = 13;
+        public static final int Back_Right_Drive_CANcoder = 14;
+    }
+
+    public static final class InputConstants {
+        public static final int kDriverControllerPort0 = 0;
+        public static final int kDriverControllerPort1 = 1;
+        public static final int kDriverControllerPort2 = 2;
+        public static final boolean fieldOrientation = true;
+        public static final double kDeadband = 0.028;
+      }
+}
