5b09a821406fb8093bfd8010cfe576fab739669a
AlphaLQ <sjb2019dpo@gmail.com>
2025-02-15 13:12:21 -0800

    commands lock and hold upon canrage detection

---
+++ src/main/java/frc/robot/commands/algae_manipulator/AlgaeIntake.java
@@ -0,0 +1,35 @@
+package frc.robot.commands.algae_manipulator;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.subsystems.AlgaeManipulator;
+
+public class AlgaeIntake extends Command{
+    AlgaeManipulator intake;
+
+    public AlgaeIntake(AlgaeManipulator intake) {
+        this.intake = intake;
+        addRequirements(intake);
+    }
+
+    @Override
+    public void initialize(){
+        intake.resetLockout();
+    }
+
+    @Override
+    public void execute() {
+        if(!intake.isLocked()){
+            intake.setAlgaeVelo(-0.7);
+        } else intake.setAlgaeVelo(-0.05);
+        
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        if(intake.isLocked()){
+            intake.setAlgaeVelo(-0.05);
+        } else intake.setAlgaeVelo(0); 
+        
+    }
+}
---
+++ src/main/java/frc/robot/commands/algae_manipulator/AlgaeOutake.java
@@ -0,0 +1,24 @@
+package frc.robot.commands.algae_manipulator;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.robot.subsystems.AlgaeManipulator;
+
+public class AlgaeOutake extends Command{
+    AlgaeManipulator outake;
+
+    public AlgaeOutake(AlgaeManipulator outake) {
+        this.outake = outake;
+        addRequirements(outake);
+    }
+
+    @Override
+    public void execute() {
+        outake.setAlgaeVelo(1);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        outake.setAlgaeVelo(0.0); 
+    }
+}
