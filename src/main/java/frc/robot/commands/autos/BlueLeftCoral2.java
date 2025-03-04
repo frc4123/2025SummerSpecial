5b540df9c3d474b3bd788c1a39fe51739afd8d32
AlphaLQ <sjb2019dpo@gmail.com>
2025-03-03 21:08:52 -0800

    auto pls work

--- src/main/java/frc/robot/commands/autos/BlueLeftCoral2.java
+++ src/main/java/frc/robot/commands/autos/BlueLeftCoral2.java
@@ -5,8 +5,8 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class BlueRightCoral extends SubsystemBase{
-    public Command blueRightCoral(){
-        return AutoBuilder.buildAuto("blueRightCoral_3");
+public class BlueLeftCoral2 extends SubsystemBase{
+    public Command blueLeftCoral(){
+        return AutoBuilder.buildAuto("blueLeftCoral_2");
     }
 }
--- src/main/java/frc/robot/commands/autos/BlueRightCoral2.java
+++ src/main/java/frc/robot/commands/autos/BlueRightCoral2.java
@@ -5,8 +5,8 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class BlueLeftCoral extends SubsystemBase{
-    public Command blueLeftCoral(){
-        return AutoBuilder.buildAuto("blueLeftCoral_3");
+public class BlueRightCoral2 extends SubsystemBase{
+    public Command blueRightCoral(){
+        return AutoBuilder.buildAuto("blueRightCoral_2");
     }
 }
