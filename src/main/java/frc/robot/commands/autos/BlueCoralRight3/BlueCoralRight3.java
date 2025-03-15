fe26ef4fd56b786e22181e6e8a88747d1e57aba1
AlphaLQ <sjb2019dpo@gmail.com>
2025-03-15 10:31:22 -0700

    3 coral pls work

---
+++ src/main/java/frc/robot/commands/autos/BlueCoralRight3/BlueCoralRight3.java
@@ -0,0 +1,12 @@
+package frc.robot.commands.autos.BlueCoralRight3;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class BlueCoralRight3 extends SubsystemBase{
+    public Command blueRightCoral(){
+        return AutoBuilder.buildAuto("BlueRightCoral3 3");
+    }
+}
